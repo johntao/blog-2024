99a0fcad4572d56c18cb84ccc3390c9e4125a1a9
John Tao <80338582+johntao@users.noreply.github.com>
2024-10-29 12:36:42 +0800

    feat: add reading time

--- src/utils/readingTime.ts
+++ src/utils/readingTime.ts
@@ -5,6 +5,7 @@ export function remarkReadingTime() {
 	return function (tree: any, { data }: any) {
 		const textOnPage = toString(tree);
 		const readingTime = getReadingTime(textOnPage);
+		// console.log(`======================================= ${readingTime.text} =======================================`)
 		data.astro.frontmatter.minutesRead = readingTime.text;
 	};
 }
