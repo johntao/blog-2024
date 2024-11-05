2f77ab8e203779c1fbf779b0a90b798a51dfebf5
John Tao <80338582+johntao@users.noreply.github.com>
2024-11-05 17:00:42 +0800

    Squashed commit of the following:
    commit 408f0649cbfe3df65241ecbc076401e36fc859a3
    commit 85b6c047648aa9c30bad641d59e97e77e70f611c
    
    feat: implment theme switch function
    refactor: make script inline

--- src/consts.ts
+++ src/consts.ts
@@ -3,4 +3,3 @@
 
 export const SITE_TITLE = 'JT.1';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
-export const LOGO_ANI_OFF = 'logo_ani_off';
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,7 +11,7 @@
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	// darkMode: 'class',
+	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
