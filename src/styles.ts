caae163e2fa3ef932821132c24191c5fd534503e
John Tao <80338582+johntao@users.noreply.github.com>
2024-11-05 17:07:37 +0800

    Squashed commit of the following:
    commit 82c02dba996c4311cc1ec5d74cfcec7878bb1171
    commit 5c5afdc1bf3ed65bd710ba9d1a83f596e6c1302e
    commit af5e126924bd67b1be544cfa710963e594eab87d
    
    feat: apply theme color scheme properly

---
+++ src/styles.ts
@@ -0,0 +1,5 @@
+const className = {
+  UI_BAR_COLOR: 'bg-neutral-100 dark:bg-neutral-900 text-neutral-700 dark:text-neutral-300',
+  CONTENT_AREA_COLOR: 'bg-white dark:bg-neutral-800 text-neutral-700 dark:text-neutral-300',
+};
+export default className;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,4 @@
-
-// const disabledCss = {
-// 	'code::before': false,
-// 	'code::after': false,
-// 	'blockquote p:first-of-type::before': false,
-// 	'blockquote p:last-of-type::after': false,
-// 	pre: false,
-// 	code: false,
-// 	'pre code': false
-// };
+const plugin = require('tailwindcss/plugin');
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -20,14 +11,9 @@ export default {
 				4.5: '1.15rem',
 				23: '5.8rem',
 			},
-			// typography: {
-			// 	DEFAULT: { css: disabledCss },
-			// 	sm: { css: disabledCss },
-			// 	lg: { css: disabledCss },
-			// 	xl: { css: disabledCss },
-			// 	'2xl': { css: disabledCss }
-			// }
 		}
 	},
-	plugins: [require('@tailwindcss/typography')]
+	plugins: [
+		require('@tailwindcss/typography'),
+	]
 };
