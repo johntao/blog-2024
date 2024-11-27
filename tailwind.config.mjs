516a4d025d4f8967532c12042173f82d37829c0e
John Tao <80338582+johntao@users.noreply.github.com>
2024-11-27 17:24:11 +0800

    chore: debugging cloudflare deploy error

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,15 @@
 const plugin = require('tailwindcss/plugin');
 
+// const disabledCss = {
+// 	'code::before': false,
+// 	'code::after': false,
+// 	'blockquote p:first-of-type::before': false,
+// 	'blockquote p:last-of-type::after': false,
+// 	pre: false,
+// 	code: false,
+// 	'pre code': false
+// };
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	darkMode: ['class'],
@@ -57,7 +67,14 @@ export default {
 					'4': 'hsl(var(--chart-4))',
 					'5': 'hsl(var(--chart-5))'
 				}
-			}
+			},
+			// typography: {
+			// 	DEFAULT: { css: disabledCss },
+			// 	sm: { css: disabledCss },
+			// 	lg: { css: disabledCss },
+			// 	xl: { css: disabledCss },
+			// 	'2xl': { css: disabledCss }
+			// }
 		}
 	},
 	plugins: [
