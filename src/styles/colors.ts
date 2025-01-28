e1384a852b1696891326a3fea1a1e60ea87054d0
John Tao <80338582+johntao@users.noreply.github.com>
2025-01-28 17:39:09 +0800

    feat: add Logo component and update theme colors in Tailwind configuration

---
+++ src/styles/colors.ts
@@ -0,0 +1,55 @@
+import colors from 'tailwindcss/colors';
+const accent = {
+  ...colors.sky,
+  // 600: '#0ea5e9',
+};
+// const gray = {
+//   100: '#f5f6f8',
+//   200: '#eceef2',
+//   300: '#c0c2c7',
+//   400: '#888b96',
+//   500: '#545861',
+//   700: '#353841',
+//   800: '#24272f',
+//   900: '#17181c'
+// };
+export default {
+  accent,
+  gray: colors.neutral,
+  // background: 'hsl(var(--background))',
+  // foreground: 'hsl(var(--foreground))',
+  // card: {
+  // 	DEFAULT: 'hsl(var(--card))',
+  // 	foreground: 'hsl(var(--card-foreground))'
+  // },
+  // popover: {
+  // 	DEFAULT: 'hsl(var(--popover))',
+  // 	foreground: 'hsl(var(--popover-foreground))'
+  // },
+  // primary: {
+  // 	DEFAULT: 'hsl(var(--primary))',
+  // 	foreground: 'hsl(var(--primary-foreground))'
+  // },
+  // secondary: {
+  // 	DEFAULT: 'hsl(var(--secondary))',
+  // 	foreground: 'hsl(var(--secondary-foreground))'
+  // },
+  // muted: {
+  // 	DEFAULT: 'hsl(var(--muted))',
+  // 	foreground: 'hsl(var(--muted-foreground))'
+  // },
+  // destructive: {
+  // 	DEFAULT: 'hsl(var(--destructive))',
+  // 	foreground: 'hsl(var(--destructive-foreground))'
+  // },
+  // border: 'hsl(var(--border))',
+  // input: 'hsl(var(--input))',
+  // ring: 'hsl(var(--ring))',
+  // chart: {
+  // 	'1': 'hsl(var(--chart-1))',
+  // 	'2': 'hsl(var(--chart-2))',
+  // 	'3': 'hsl(var(--chart-3))',
+  // 	'4': 'hsl(var(--chart-4))',
+  // 	'5': 'hsl(var(--chart-5))'
+  // }
+}
